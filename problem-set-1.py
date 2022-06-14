cb60b620c23bb34ae7f38ba11e8c1ac92f0b7621
rosie <rosie.ayvn@gmail.com>
2022-06-14 21:45:31 +0400

    kid's stuff

---
+++ problem-set-1.py
@@ -0,0 +1,52 @@
+#Homework1
+
+#Problem 1
+
+x = 17
+y = 6
+
+q = 17/6
+r = 17 % 6
+q = int(q)
+
+print(x, "=", q, "*", y, "+", r)
+#Bravooooooooooooooooooooooooooooo
+
+#Problem 2
+
+tiv = 3+4j
+module = 3*3 + 4*4
+module **= 1/2 
+
+print(module)
+
+#Problem 3
+#Antashyan poem is not yet give, => an amazing excuse to skip this one
+
+#Problem 4
+
+tiv1 = 3
+tiv2 = 3.14
+
+temp = tiv2
+tiv2 = tiv1
+tiv1 = temp
+
+print(tiv1, tiv2)
+
+#Problem 5
+
+#Problem 6
+
+initprice = 1300
+change1 = initprice - (initprice*5)/100
+change2 = change1 + (change1*10)/100
+finalprice = change2
+
+print("The final price for a cup of flat white is",finalprice, "drams")
+
+#Q:Kebab style variable
+
+
+
+
